6c1af1ddee12c932a59f2718fd6b24afa20b7c7b
subhramajumdar <subhramajumdar18@gmail.com>
2019-09-29 14:44:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         // check if player won the game
-        if (scores[activePlayer] >= 50) {
+        if (scores[activePlayer] >= 20) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
